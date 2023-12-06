9dfb3314eef74163cb80a474e0ba9d8f5a6803b9
PabloMuh <115596073+PabloMuh@users.noreply.github.com>
2023-12-06 17:03:01 -0300

    Add files via upload

--- teste.py
+++ teste.py
@@ -1,5 +1,6 @@
 import os
 import time
+import requests
 
 account_list = []
 
@@ -22,6 +23,7 @@ class account():
             print(f"you make a withdraw of {value} in your account. Your account balance now is {self.balance}")
         else:
             print("You are unable to withdraw this value")
+
     def show_transactions(self):
         for transactions in self.history:
             print(transactions)
@@ -31,6 +33,7 @@ class account():
             self.balance -= value
             destine.balance += value
             self.history.append(f"You transferred {value} to account {destine.code}")
+            destine.history.append(f"You received {value} of the account {self.code}")
             print(f"Transfer of {value} to account {destine.code} completed successfully.")
         else:
             print("You do not have sufficient funds for this transfer.")
@@ -38,6 +41,15 @@ class account():
 
 def clear_terminal():
     os.system('cls' if os.name == 'nt' else 'clear')
+
+def download_file(url, filename):
+    response = requests.get(url)
+    if response.status_code == 200:
+        with open(filename, 'wb') as file:
+            file.write(response.content)
+        print(f"Now you have Downloaded the cheque book")
+    else:
+        print(f"Failed to download file. Status code: {response.status_code}")
 
 def access_account(user):
     while True:
@@ -48,35 +60,53 @@ def access_account(user):
         print("3 - See my history")
         print("4 - Tranference")
         print("5 - See my current balance")
-        print("6 - Quit account")
+        print("6 - Request a check book")
+        print("7 - Quit account")
+
         choice = int(input("Select your choice: "))
+
         if choice == 1:
+
+            clear_terminal()
             value = int(input("How many is the value of deposit: "))
             user.deposit(value)
+
         elif choice == 2:
+            clear_terminal()
             value = int(input("How many is the value of withdraw: "))
             user.withdraw(value)
+
         elif choice == 3:
             clear_terminal()
             user.show_transactions()
+
         elif choice == 4:
+            clear_terminal()
             code = input("Enter the account code you want to transfer:")
             check_transfer = False
             for obj in account_list:
                 if code == obj.code:
                     check_transfer = True
                     break  
+
             if check_transfer:
                 value = int(input("How much is the value of transfer: "))
                 user.transfer(obj, value)
+
             else:
+                clear_terminal()
                 print("Account not found, you will be redirected to your home page!!!")
                 time.sleep(3)          
-                    
+                clear_terminal()    
+
         elif choice == 5:
             clear_terminal()
             print(f"Your Current balance is: {user.balance}")
         elif choice == 6:
+            clear_terminal()
+            download_file(url,filename)
+            
+        elif choice == 7:
             clear_terminal()
             break
         else:
@@ -86,10 +116,17 @@ def access_account(user):
         clear_terminal()
 
 
+
+
+# Example usage:
+url = 'https://static.vecteezy.com/system/resources/thumbnails/004/566/136/small/bank-check-cheque-template-free-vector.jpg'
+filename = 'cheque.jpg'
+
+
 check = True
 
 while True:
-
+    clear_terminal()
     print("Welcome to Metropolitan Bank")
     print("What do you want to do?")
     print("1 - register an account")
@@ -107,14 +144,17 @@ while True:
         while any(check_object.code == obj.code for obj in account_list):
             code = input("This code account already exists, please choose another combination: ")
             check_object = account(code,password)
+
         account_list.append(check_object)
         print("Account created successfully")
         time.sleep(3)
         clear_terminal()
+
     elif choice == 2:
         clear_terminal()
         code = input("Type your code account: ")
         password = input("Type your Password: ")
+
         for obj in account_list:
             if code == obj.code and password == obj.password:
                 clear_terminal()
@@ -127,10 +167,12 @@ while True:
             clear_terminal()
 
         check = True
+
     elif choice == 3:
         clear_terminal()
         print("Thanks for using our services, come back soon!!!")
         break
+
     else:
         clear_terminal()
         print("invalid operation,select another")
