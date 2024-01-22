70fc0824259218bf6100ba70b69c365ddb98555d
PabloMuh <115596073+PabloMuh@users.noreply.github.com>
2024-01-21 22:54:59 -0300

    Add files via upload

--- projectOO/bank.py
+++ projectOO/bank.py
@@ -5,70 +5,73 @@ filename = 'cheque.jpg'
 
 def access_account(user,account_list):
     while True:
-        print("Hello User, Welcome to central administration, What operation do you want to do?")
-        print("1 - Deposit")
-        print("2 - Withdraw")
-        print("3 - See my history")
-        print("4 - Tranference")
-        print("5 - See my current balance")
-        print("6 - Request a check book")
-        print("7 - Pay bills")
-        print("8 - Convert my money to another currency")
-        print("9 - loan")
-        print("10 - Investments")
-        print("11 - Quit account")
-
-        choice = int(input("Select your choice: "))
-
-        if choice == 1:
+        while True:
+            try:
+                print("Hello User, Welcome to central administration, What operation do you want to do?")
+                print("1 - Deposit")
+                print("2 - Withdraw")
+                print("3 - See my history")
+                print("4 - Tranference")
+                print("5 - See my current balance")
+                print("6 - Request a check book")
+                print("7 - Pay bills")
+                print("8 - Convert my money to another currency")
+                print("9 - loan")
+                print("10 - Investments")
+                print("11 - Quit account")
+                choice = int(input("Select your choice: "))
+                break
+            except ValueError:
+                clear_terminal()
+                print("Must be a Integer")
+                time.sleep(3)
+        try:
             clear_terminal()
-            value = int(input("How many is the value of deposit: "))
-            user.deposit(value)
 
-        elif choice == 2:
-            clear_terminal()
-            value = int(input("How many is the value of withdraw: "))
-            user.withdraw(value)
+            if choice == 1:  
+                value = input("How many is the value of deposit: ")
+                user.deposit(value)
 
-        elif choice == 3:
-            clear_terminal()
-            user.show_transactions()
+            elif choice == 2:
+                value = input("How many is the value of withdraw: ")
+                user.withdraw(value)
 
-        elif choice == 4:
-            user.transfer(account_list)    
+            elif choice == 3:
+                user.show_transactions()
 
-        elif choice == 5:
-            clear_terminal()
-            if user.currency == 1:
-                print(f"Your Current balance is: {user.balance} reais")
-            elif user.currency == 2:
-                print(f"Your Current balance is: {user.balance} dollars")
-            elif user.currency == 3:
-                print(f"Your Current balance is: {user.balance} euros")
+            elif choice == 4:
+                user.transfer(account_list)    
 
-        elif choice == 6:
-            clear_terminal()
-            download_file(url,filename)
+            elif choice == 5:
+                if user.currency == 1:
+                    print(f"Your Current balance is: {user.balance} reais")
+                elif user.currency == 2:
+                    print(f"Your Current balance is: {user.balance} dollars")
+                elif user.currency == 3:
+                    print(f"Your Current balance is: {user.balance} euros")
 
-        elif choice == 7:
-            clear_terminal()
-            user.bills()
+            elif choice == 6:
+                download_file(url,filename)
 
-        elif choice == 8:
-            user.convert()
+            elif choice == 7:
+                user.bills()
 
-        elif choice == 9:
-            user.loan()
+            elif choice == 8:
+                user.convert()
 
-        elif choice == 10:
-            user.investiment()
-            
-        elif choice == 11:
-            clear_terminal()
-            break
+            elif choice == 9:
+                user.loan()
 
-        else:
-            clear_terminal()
-            print("Invalid choice, please select another")
+            elif choice == 10:
+                user.investiment()
+                
+            elif choice == 11:
+                clear_terminal()
+                break
+            else:
+                print("Invalid choice, please select another")
+        except AbortTransaction as error:
+            print(error)
+        
         time.sleep(3)
         clear_terminal()
